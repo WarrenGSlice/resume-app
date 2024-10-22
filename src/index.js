ab2e862b37ba4d6a9fa662cd440e3be692ce531a
kylehton <kyleton06@gmail.com>
2024-10-22 14:27:36 -0700

    added in a separate js component for google sign in, but could not get client id to validate access to login

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App';
+import Homepage from './Homepage'; // changed to open homepage on initial load
 
 const container = document.querySelector('#root');
 const root = createRoot(container);
-root.render(<App />);
+root.render(<Homepage />); // changed to open homepage on initial load
